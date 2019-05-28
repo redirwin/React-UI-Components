7b50809dbfab30d58a108572715e4e5cbb3508f6
Dave <redirwin@gmail.com>
2019-05-28 08:54:24 -0700

    WIP - header modules created and imported to HeaderContainer.

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => {
+	return (
+		<a href="https://www.reactjs.org">
+			<h1>Get started with React</h1>
+			<p>
+				React makes it painless to creae interactive
+				UIs. Design simple views for each state in your
+				application.
+			</p>
+		</a>
+	);
+};
+
+export default CardContent;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,19 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+	return (
+		<div>
+			<div className="school-name">Lambda School</div>
+			<a
+				className="handle"
+				href="https://twitter.com/LambdaSchool"
+			>
+				@LambdaSchool
+			</a>
+			<div className="date">28 May</div>
+		</div>
+	);
+};
+
+export default HeaderTitle;
